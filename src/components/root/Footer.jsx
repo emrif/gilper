2771e0e67cd9fca9af25ceca278b738710a46e27
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-11-28 17:59:59 +0600

    Added a Neko text

--- src/components/root/Footer.jsx
+++ src/components/root/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <footer className="w-auto bg-secondary h-44">gliper@gmail.com Email</footer>;
+  return <footer className="w-auto bg-secondary h-44">gliper@gmail.com Neko</footer>;
 };
 
 export default Footer;
