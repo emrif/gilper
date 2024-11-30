884087262f732457d61967a8be270f173ceac0c1
Ruhannn <ruhanrouf17@gmail.com>
2024-11-30 20:15:14 +0600

    fixed env and loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,20 @@ export default {
         primary: "var(--primary)",
         secondary: "var(--secondary)",
         accent: "var(--accent)",
+      }, keyframes: {
+        boo: {
+          '0%': {
+            opacity: '1',
+            transform: 'translate3d(0, 0, 0)',
+          },
+          '100%': {
+            opacity: '0.3',
+            transform: 'translate3d(0, -1rem, 0)',
+          },
+        },
+      },
+      animation: {
+        boo: '0.9s boo infinite alternate',
       },
     },
   },
